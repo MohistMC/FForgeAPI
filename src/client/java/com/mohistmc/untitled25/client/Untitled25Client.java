c4fc313260eeae70c3dd0903603864ba8df4af47
Mgazul <mgazul@163.com>
2024-08-24 04:07:45 +0800

    Init code

---
+++ src/client/java/com/mohistmc/untitled25/client/Untitled25Client.java
@@ -0,0 +1,10 @@
+package com.mohistmc.untitled25.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Untitled25Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/mohistmc/untitled25/Untitled25.java
@@ -0,0 +1,10 @@
+package com.mohistmc.untitled25;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Untitled25 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
